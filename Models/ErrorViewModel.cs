b26986d257b01fe8cba862b98682b21f348c76fe
Chopapik <123145310+Chopapik@users.noreply.github.com>
2025-03-15 14:31:07 +0100

    Add initial files

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace auction_portal_ubb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
