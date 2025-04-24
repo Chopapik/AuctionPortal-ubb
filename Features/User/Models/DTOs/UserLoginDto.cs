3e750e54a207129f3a9fb7075110e7ba800c5f73
Chopapik <123145310+Chopapik@users.noreply.github.com>
2025-04-24 17:07:09 +0200

    Create UserLoginDto.cs

---
+++ Features/User/Models/DTOs/UserLoginDto.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace auction_portal_ubb.Features.User.Models.DTOs
+{
+    public class UserLoginDto
+    {
+        
+    }
+}
