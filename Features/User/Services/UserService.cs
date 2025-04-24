61e43cfa82d715ecb5665c610bf4c738c3f202fd
Chopapik <123145310+Chopapik@users.noreply.github.com>
2025-04-24 17:08:24 +0200

    Add Crud and login services

--- Features/User/Services/UserService.cs
+++ Features/User/Services/UserService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using auction_portal_ubb.Features.User.Models.DTOs;
 using auction_portal_ubb.Models;
+using Microsoft.IdentityModel.Tokens;
 
 namespace auction_portal_ubb.Features.User.Services
 {
@@ -48,6 +49,83 @@ namespace auction_portal_ubb.Features.User.Services
             {
                 throw new Exception("Error occurred while registering the user.", ex);
             }
+        }
+
+
+        public UserModel GetUser(int userId)
+        {
+            try
+            {
+                var foundUser = _context.Users.Find(userId);
+
+                if (foundUser == null)
+                {
+                    throw new Exception("User not found");
+                }
+
+                return foundUser;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception("Error occurred while retrieving the user.", ex);
+            }
+        }
+
+
+        public UserModel UpdateUser(UserUpdateDto userUpdateData)
+        {
+            try
+            {
+                var userToUpdate = _context.Users.Find(userUpdateData.Id);
+
+                if (userToUpdate == null)
+                {
+                    throw new Exception("User not found");
+                }
+
+                userToUpdate.Name = string.IsNullOrEmpty(userUpdateData.Name) ? userToUpdate.Name : userUpdateData.Name;
+                userToUpdate.Surname = string.IsNullOrEmpty(userUpdateData.Surname) ? userToUpdate.Surname : userUpdateData.Surname;
+                userToUpdate.Email = string.IsNullOrEmpty(userUpdateData.Email) ? userToUpdate.Email : userUpdateData.Email;
+                userToUpdate.PhoneNumber = string.IsNullOrEmpty(userUpdateData.PhoneNumber) ? userToUpdate.PhoneNumber : userUpdateData.PhoneNumber;
+
+
+                // save to db
+                _context.SaveChanges();
+
+                return userToUpdate;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occurred while updating the user.", ex);
+            }
+        }
+
+
+        public void DeleteUser(int userId)
+        {
+            try
+            {
+                var userToDelete = _context.Users.Find(userId);
+
+                if (userToDelete == null)
+                {
+                    throw new Exception("User not found");
+                }
+
+                _context.Users.Remove(userToDelete);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error occurred while deleting the user.", ex);
+            }
+        }
+
+        public bool LoginUser(string username, string password)
+        {
+            // Implementacja logowania użytkownika
+            return true;
         }
 
     }
