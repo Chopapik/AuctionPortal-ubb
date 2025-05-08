2fc3e9326e2f6d4a03ed1173d1f7688d91f400da
Chopapik <123145310+Chopapik@users.noreply.github.com>
2025-05-08 17:52:50 +0200

    Update UserService.cs

--- Features/User/Services/UserService.cs
+++ Features/User/Services/UserService.cs
@@ -10,22 +10,46 @@ namespace auction_portal_ubb.Features.User.Services
     public class UserService
     {
         private readonly IUserRepository _userRepository;
+        private readonly IAddressRepository _addressRepository;
+        private readonly UserProfileViewModel userProfileViewModel;
 
-        public UserService(IUserRepository userRepository)
+
+        public UserService(IUserRepository userRepository, IAddressRepository addressRepository)
         {
             _userRepository = userRepository;
+            _addressRepository = addressRepository;
         }
-        public async Task<UserModel> GetAccountData(int userId)
+        public async Task<UserProfileViewModel> GetAccountData(int userId)
         {
             try
             {
                 var user = await _userRepository.GetById(userId);
+                var address = await _addressRepository.GetByUserId(userId);
                 if (user == null)
                 {
                     throw new Exception("User not found.");
                 }
 
-                return user;
+                if (address == null)
+                {
+                    throw new Exception("Address not found.");
+                }
+
+                return new UserProfileViewModel
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Surname = user.Surname,
+                    Email = user.Email,
+                    Nick = user.Nick,
+                    PhoneNumber = user.PhoneNumber,
+                    Street = address.Street,
+                    BuildingNumber = address.BuildingNumber,
+                    ApartmentNumber = address.ApartmentNumber,
+                    ZipCode = address.ZipCode,
+                    City = address.City,
+                    Country = address.Country,
+                };
             }
             catch (Exception ex)
             {
@@ -34,11 +58,17 @@ namespace auction_portal_ubb.Features.User.Services
         }
 
 
-        public async Task<UserModel> UpdateAccountData(UserUpdateDto userUpdateData)
+        public async Task<int> UpdateAccountData(UserProfileUpdateViewModel userProfileUpdateViewModel)
         {
             try
             {
-                return await _userRepository.UpdateAccount(userUpdateData);
+                var userUpdateData = userProfileUpdateViewModel.User;
+                var addressUpdateData = userProfileUpdateViewModel.Address;
+
+                var user = await _userRepository.UpdateAccount(userUpdateData);
+                await _addressRepository.Update(addressUpdateData, userUpdateData.Id);
+
+                return user.Id;
             }
             catch (Exception ex)
             {
